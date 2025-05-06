c6b9a587c92d224f5e258c57ca9d4e4b9d58f662
aivanov <aivanov@mobiledep.ru>
2025-05-06 18:59:21 +0300

    удалил секретный ключ в коде

--- users/transactions.py
+++ users/transactions.py
@@ -1,17 +1,23 @@
 import stripe
+from . import dao
+from .schemas import TransactionInfo
 
 
 def checkout_transaction(session_id: str):
-    # if session_id in bd:
-        # return
+    if session_id == dao.get_id_transaction(transaction_id=session_id):
+        return
 
     checkout_session = stripe.checkout.Session.retrieve(
         session_id,
         expand=['line_items'],
     )
 
+    transaction_info = TransactionInfo.model_validate(obj=checkout_session)
+
     if checkout_session.payment_status == 'paid':
-        # save transactions
-        # save additional user info
-        # save astrals user
-        pass
+        dao.create_transactions(values=transaction_info)
+        dao.update_user_info(values={'email': transaction_info.email, 'country': transaction_info.country},
+                             user_id=transaction_info.client_reference_id)
+        astrals = dao.get_user_astrals(user_id=transaction_info.client_reference_id)
+        astrals += transaction_info.quantity
+        dao.update_astrals(values={'astrals': astrals}, user_id=transaction_info.client_reference_id)
